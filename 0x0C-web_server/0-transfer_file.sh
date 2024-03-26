3fca9011e8b7b5b4f9752dde804b9cae54f985f8
tAf <muzirat0@gmail.com>
2024-03-27 00:08:59 +0200

    0. Transfer a file to your server v1.2

--- 0x0C-web_server/0-transfer_file.sh
+++ 0x0C-web_server/0-transfer_file.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # transfers a file to remote server using scp
-if [ "$#" -lt 4 ]; then
-  echo "Usage: 0-transfer_file PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY"
+
+path_to_file="$1"
+ip_address="$2"
+username="$3"
+path_to_sshkey="$4"
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: 0-transfer_file PATH_TO_FILE IP USERNAME PATH_TO_SSH_KEY"
 else
-  scp -i "${4}" -o StrictHostKeyChecking=no "${1}" "${3}@${2}:~/"
+    scp -o StrictHostKeyChecking=no -i "$path_to_sshkey" "$path_to_file" "$username"@"$ip_address":~/
 fi
+
