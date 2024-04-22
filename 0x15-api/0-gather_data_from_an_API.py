8c21ec5b0f9fa0bb77914a0b62d92d817baa9b23
tAf <muzirat0@gmail.com>
2024-04-22 21:26:54 +0200

    0. Gather data from an API new style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,18 @@ import sys
 
 if __name__ == '__main__':
     url_list = 'https://jsonplaceholder.typicode.com/todos'
-    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_users = 'https://jsonplaceholder.typicode.com/users/{}'
 
     user_id = sys.argv[1]
+    user_response = requests.get(url_users.format(2))
     response_list = requests.get(url_list)
-    response_users = requests.get(url_users)
 
+    user = user_response.json()
     todo_list = response_list.json()
-    users_list = response_users.json()
 
     users_todo_list = [i for i in todo_list if i['userId'] == int(user_id)]
     done_tasks = [i for i in users_todo_list if i['completed']]
-    user = [i for i in users_list if i['id'] == int(user_id)]
-    user_name = user[0]['name']
+    user_name = user['name']
 
     print_str = 'Employee {} is done with tasks({}/{}):'
     print(print_str.format(user_name, len(done_tasks), len(users_todo_list)))
