4fea0f45d1e63bbbcc498b94640006b69c67a2be
tAf <muzirat0@gmail.com>
2024-04-22 21:54:15 +0200

    0. Gather data from an API new style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,22 +8,21 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    url_list = 'https://jsonplaceholder.typicode.com/todos'
-    url_users = 'https://jsonplaceholder.typicode.com/users'
-
     user_id = sys.argv[1]
-    response_list = requests.get(url_list)
-    response_users = requests.get(url_users)
 
-    todo_list = response_list.json()
-    users_list = response_users.json()
+    todo_list = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.
+            format(user_id)
+            ).json()
+    user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.
+            format(user_id)
+            ).json()
 
-    users_todo_list = [i for i in todo_list if i['userId'] == int(user_id)]
-    done_tasks = [i for i in users_todo_list if i['completed']]
-    user = [i for i in users_list if i['id'] == int(user_id)]
-    user_name = user[0]['name']
+    done_tasks = [i.get('title') for i in todo_list if i.get('completed')]
+    user_name = user.get('name')
 
     print_str = 'Employee {} is done with tasks({}/{}):'
-    print(print_str.format(user_name, len(done_tasks), len(users_todo_list)))
+    print(print_str.format(user_name, len(done_tasks), len(todo_list)))
     for task in done_tasks:
-        print('\t{}'.format(task['title']))
+        print('\t{}'.format(task))
