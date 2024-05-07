ef1affcfb5a18c0ce7d6c5478904e7fbdbf051be
tAf <muzirat0@gmail.com>
2024-05-07 14:30:31 +0200

    0. How many subs? v2
    header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,13 @@ subscriber number module
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ returns subcsriber number """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'User'}
 
-    response = requests.get(url).json()
+    response = requests.get(url, headers=header).json()
     subscribers = response['data']['subscribers']
     if subscribers:
         return subscribers
